6e8604e4f0e378d8d68cbe809130f61152df9dd6
Otakar Andrysek <otakarandrysek@Otakars-MacBook-Pro.local>
2019-10-22 16:06:43 -0700

    Finished Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Caching
+## Otakar Andrysek - 10/22/2019 - Intro to R
+## These Functions are used to calculate the inverse of a square matrix. 
+## Because these calculations can sometimes be quite intense,
+## cacheing is used to save and re-use already solved matricies.
 
-## Write a short comment describing this function
-
+# This function creates the cached matrix symbol. The inverse is initially
+# set to NULL and is calculated and stored upon initial computation.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set=set, get=get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
+# This function returns a matrix that is the inverse of 'x'. If the
+# function is using a cached inverse it will be printed to the console.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  print(inv)
+  if(!is.null(inv)) {
+    message("Getting cashed data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setInv(inv)
+  inv
 }
+#EOF
